7a486f0cd3f99d6f52ad59143a472f3f7dc7b888
Aman Abdirazak <amanabdirizak@icloud.com>
2022-12-16 12:57:58 -0600

    got everything sourced and server running

---
+++ server/routes/tasks.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const tasksRouter = express.Router();
+const pool = require('../modules/pool.js');
+
+//GET
+
+
+
+
+//POST
+
+
+
+
+
+
+//PUT
+
+
+
+
+
+
+//DELETE
+
+
+
+
+
+
+
+module.exports = tasksRouter;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+//forward to routes folder
+app.use('/tasks', tasksRouter)
+
+//listen for server on PORT 5000
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+  });
