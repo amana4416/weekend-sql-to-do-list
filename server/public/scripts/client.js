4d465dc588f0c09cd3c7ce44e530e35c5f9a8f87
Aman Abdirazak <amanabdirizak@icloud.com>
2022-12-16 20:14:10 -0600

    GET is working, able to see tasks on the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,21 +2,53 @@ $( document ).ready(onReady);
 
 function onReady() {
     console.log('jq');
-   createTask();
+   //load existing tasks on page as soon as server is opened
+   getTask();
+   //createTask
 }
 
 //will turn input values into an object we can send to server
 function createTask() {
-    $( '#submitButton' ).on( 'click', function(){
+    $( '#submitButton' ).on('click', function(){
         console.log( 'adding task' );
     //grab input values
     let newTask = $('#taskInput').val();
     let newNote = $('#noteInput').val();
+
     //make a new object to send to server
     let taskToSend = {
         task: newTask,
-        note: newNote
+        note: newNote,
+        complete: false
     };
     //call the function that will post this object to server
-    function postTask(taskToSend);
+    // function postTask(taskToSend);
+    })
+}
+
+//function will append data from database
+function getTask() {
+    console.log('in GET /task');
+    //send request to server to send tasks to client
+    $.ajax({
+        method: 'GET',
+        url: `/tasks`
+    }).then((res) => {
+        //empty out section and append a new updated list each time
+        $('#taskList').empty();
+        //display tasks on DOM
+        for (let task of res) {
+            $('#taskList').append(`
+                <li>
+                    <h4>${task.name}</h4>
+                    <p> ${task.notes} </p>
+                    <button id="deleteButton">Delete</button>
+                    <button id="markCompleteButton"> mark complete</button>
+                </li>
+            `)
+        }
+    }).catch((err) => {
+        console.log('something broke in GET /tasks', err);
+
+    })
 }
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -3,7 +3,20 @@ const tasksRouter = express.Router();
 const pool = require('../modules/pool.js');
 
 //GET
-
+tasksRouter.get('/', (req, res) => {
+    let sqlQuery = `
+    SELECT * FROM "tasks"
+        ORDER BY "id";
+    `;
+    pool.query(sqlQuery)
+    .then((dbRes) => {
+        //send back the array of tasks
+        res.send(dbRes.rows);
+    }).catch ((dbErr) => {
+        console.log('something broke in /tasks GET');
+        res.sendStatus(500);
+    })
+})
 
 
 
