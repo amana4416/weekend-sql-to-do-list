9e9d2cafede44cce4ed2c692f5b8af32d75bdeac
Aman Abdirazak <amanabdirizak@icloud.com>
2022-12-16 19:41:37 -0600

    created a function that takes input values and stuffs it into an object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,21 @@ $( document ).ready(onReady);
 
 function onReady() {
     console.log('jq');
+   createTask();
+}
+
+//will turn input values into an object we can send to server
+function createTask() {
+    $( '#submitButton' ).on( 'click', function(){
+        console.log( 'adding task' );
+    //grab input values
+    let newTask = $('#taskInput').val();
+    let newNote = $('#noteInput').val();
+    //make a new object to send to server
+    let taskToSend = {
+        task: newTask,
+        note: newNote
+    };
+    //call the function that will post this object to server
+    function postTask(taskToSend);
 }
